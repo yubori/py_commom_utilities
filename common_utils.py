3bf5bc2df8e329e8172276072189fa17b0626e49
enokibori <enokibori@i.nagoya-u.ac.jp>
2024-04-18 13:55:54 +0900

    Replace pandas.DataFrame.append

--- common_utils.py
+++ common_utils.py
@@ -1,3 +1,8 @@
+import pandas as pd
+import numpy as np
+import platform
+import locale
+
 def check_range(val_name, val, min_, max_):
     if val < min_ or max_ < val:
         raise ValueError("Invalid '{}': range is [{},{}] but {}".format(val_name, min_, max_, val))
@@ -20,19 +25,14 @@ def resample_df(df, ix_to):
     df
         resampled pandas.DataFrame
     """
-    import pandas as pd
-    import numpy as np
     new_ix = ix_to[np.invert(np.in1d(ix_to, df.index))]  # remove duplicated index
-    tmp_df = df.append(pd.DataFrame(np.repeat(np.NaN, len(new_ix)), index=new_ix))
+    tmp_df = pd.concat([df, pd.DataFrame(np.repeat(np.NaN, len(new_ix)), index=new_ix)])
     return tmp_df.sort_index().interpolate('index').loc[ix_to]
 
 
 def set_jp_locale():
-    import platform
-    import locale
     jp_locale_str = 'ja_JP.UTF-8'
     if platform.system() == 'Windows':
         jp_locale_str = 'Japanese_Japan.932'
 
     locale.setlocale(locale.LC_TIME, f'{jp_locale_str}')
-
